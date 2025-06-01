2009364f5d4629aa6b6ff115d70c32087e284c9a
DipakFoxberry <dipak.f@foxberry.in>
2025-06-01 16:28:35 +0530

    Update layout.js

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
     >
