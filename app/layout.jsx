3b4e47f50ac580131f57ff2ef2563533cf477c24
Dipak <dipak.f@foxberry.in>
2026-01-12 17:31:01 +0530

    tittle chnage

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ import { Toaster } from 'react-hot-toast';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'AgriTraders',
+  title: 'Vijay Traders',
   description: 'Agricultural Trading Management System',
 };
 
